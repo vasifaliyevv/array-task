e686ccc1c7934edd495262c21bc56c5d936955da
vasifaliyevv <tu77c8kgt@code.edu.az>
2022-11-01 23:24:29 +0400

    added js and c# file

---
+++ array/arrays/Program.cs
@@ -0,0 +1,57 @@
+﻿#region ozumcun
+//int[] arr = { 12, 13, 1, 10, 34, 1 };
+//int n = arr.Length;
+//int smallest = Int32.MaxValue;
+//// traversing the array to find
+//// smallest element.
+//for (int i = 0; i < n; i++)
+//{
+//    if (arr[i] < smallest)
+//    {
+//        smallest = arr[i];
+//    }
+//}
+//Console.WriteLine("smallest element is: " + smallest);
+#endregion
+
+
+
+#region task1
+//int[] arr = { 25, 11, 7, 75, 56 };
+//int min = arr[0];
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] < min)
+//    {
+//        min = arr[i];
+//    }
+//}
+//Console.WriteLine("ən kiçik element: " + min);
+#endregion
+
+#region task2
+
+//int[] num ={ 25, 11, 7, 75, 5656 };
+//int min = num[0];
+//int count = 0;
+//for (int i = 0; i < num.Length; i++)
+//{
+//    if (num[i] > min)
+//    {
+//        min = num[i];
+//    }
+
+//}
+//while (min > 1)
+//{
+//    min /= 10;
+//    count++;
+//}
+
+
+//Console.WriteLine(count);
+#endregion
+
+
+
+
